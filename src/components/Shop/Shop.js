1990686064acf1b889fcdea1a35ea3b2ceafdc50
Palak-SweetCoder <pspalak6@gmail.com>
2022-10-25 11:36:44 +0600

    pagination done with add to cart problem solution

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
-import useProducts from '../../hooks/useProducts';
-import { addToDb, getStoredCart } from '../../utilities/fakedb';
+import useCart from '../../hooks/useCart';
+import { addToDb } from '../../utilities/fakedb';
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
 const Shop = () => {
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useCart();
     const [pageCount, setPageCount] = useState(0);
     const [page, setPage] = useState(0);
     const [size, setSize] = useState(10);
@@ -29,22 +29,8 @@ const Shop = () => {
             })
     }, []);
 
-    useEffect(() => {
-        const storedCart = getStoredCart();
-        const savedCart = [];
-        for (const id in storedCart) {
-            const addedProduct = products.find(product => product._id === id);
-            if (addedProduct) {
-                const quantity = storedCart[id];
-                addedProduct.quantity = quantity;
-                savedCart.push(addedProduct);
-            }
-        }
-        setCart(savedCart);
-    }, [products])
-
     const handleAddToCart = (selectedProduct) => {
-        console.log(selectedProduct);
+        // console.log(selectedProduct);
         let newCart = [];
         const exists = cart.find(product => product._id === selectedProduct._id);
         if (!exists) {
@@ -85,7 +71,7 @@ const Shop = () => {
                         key={number}
                         className={page === number ? 'selected' : ''}
                         onClick={() => setPage(number)}
-                    >{number}</button>)
+                    >{number + 1}</button>)
                 }
                 <select defaultValue={10} onChange={e => setSize(e.target.value)}>
                     <option value="5">5</option>
